39aeb3fbfd01cecddb00d104802b71f4308e6cda
Jeroen Rijks <jeroen-rijks@hotmail.com>
2018-11-29 12:26:21 +0000

    remove unused imports

--- src/main/java/uk/gov/hmcts/reform/em/annotation/rest/RectangleResource.java
+++ src/main/java/uk/gov/hmcts/reform/em/annotation/rest/RectangleResource.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.em.annotation.rest;
 
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
