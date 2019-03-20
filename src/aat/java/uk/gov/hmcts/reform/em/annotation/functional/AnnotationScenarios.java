f03cc31c573b092aafacef2a48e8c76c4c04daa2
Linus Norton <linusnorton@gmail.com>
2019-03-20 12:05:54 +0000

    remove Idam cient (#14)

--- src/aat/java/uk/gov/hmcts/reform/em/annotation/functional/AnnotationScenarios.java
+++ src/aat/java/uk/gov/hmcts/reform/em/annotation/functional/AnnotationScenarios.java
@@ -4,51 +4,22 @@ import io.restassured.RestAssured;
 import org.hamcrest.Matchers;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
-import uk.gov.hmcts.reform.em.annotation.Application;
 import uk.gov.hmcts.reform.em.annotation.testutil.Env;
-import uk.gov.hmcts.reform.em.annotation.testutil.IdamHelper;
-import uk.gov.hmcts.reform.em.annotation.testutil.S2sHelper;
+import uk.gov.hmcts.reform.em.annotation.testutil.TestUtil;
 
 import static org.hamcrest.CoreMatchers.*;
 import java.util.UUID;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class, properties = "SpringBootTest")
-@TestPropertySource(locations = "classpath:application-aat.yaml")
-@ActiveProfiles("aat")
 public class AnnotationScenarios {
 
-    @Autowired
-    private IdamHelper idamHelper;
-
-    @Autowired
-    private S2sHelper s2sHelper;
-
-    private String idamAuth;
-    private String s2sAuth;
-
-    @Before
-    public void setup() {
-        idamAuth = idamHelper.getIdamToken();
-        s2sAuth = s2sHelper.getS2sToken();
-
-        RestAssured.useRelaxedHTTPSValidation();
-    }
+    TestUtil testUtil = new TestUtil();
 
     @Test
     public void testGetAnnotationSets() throws Exception {
-        RestAssured.given()
-            .header("Authorization", idamAuth)
-            .header("ServiceAuthorization", s2sAuth)
+        testUtil
+            .authRequest()
             .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
             .request("GET", Env.getTestUrl() + "/api/annotation-sets")
             .then()
@@ -65,9 +36,8 @@ public class AnnotationScenarios {
         jsonObject.put("documentId", UUID.randomUUID().toString());
         jsonObject.put("id", newAnnotationSetId.toString());
 
-        RestAssured.given()
-            .header("Authorization", idamAuth)
-            .header("ServiceAuthorization", s2sAuth)
+        testUtil
+            .authRequest()
             .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .body(jsonObject.toString())
@@ -102,9 +72,8 @@ public class AnnotationScenarios {
         rectangles.put(0, rectangle);
         createAnnotations.put("rectangles", rectangles);
 
-        RestAssured.given()
-            .header("Authorization", idamAuth)
-            .header("ServiceAuthorization", s2sAuth)
+        testUtil
+            .authRequest()
             .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
                 .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
                 .body(createAnnotations)
@@ -123,9 +92,8 @@ public class AnnotationScenarios {
         comment.put("content", "text2");
         rectangle.put("height", 13f);
 
-        RestAssured.given()
-            .header("Authorization", idamAuth)
-            .header("ServiceAuthorization", s2sAuth)
+        testUtil
+            .authRequest()
             .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
                 .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
                 .body(createAnnotations)
@@ -141,17 +109,15 @@ public class AnnotationScenarios {
                 .body("comments[0].content", is("text2"))
                 .body("comments", Matchers.hasSize(1));
 
-        RestAssured.given()
-            .header("Authorization", idamAuth)
-            .header("ServiceAuthorization", s2sAuth)
+        testUtil
+            .authRequest()
             .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
                 .request("DELETE", Env.getTestUrl() + "/api/annotations/" + annotationId.toString() )
                 .then()
                 .statusCode(200);
 
-        RestAssured.given()
-            .header("Authorization", idamAuth)
-            .header("ServiceAuthorization", s2sAuth)
+        testUtil
+            .authRequest()
             .header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
                 .request("GET", Env.getTestUrl() + "/api/annotations/" + annotationId.toString() )
                 .then()
