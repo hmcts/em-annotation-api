4f770ce4aebfa6e88793bf631db6051388f684d6
Jeroen Rijks <jeroen-rijks@hotmail.com>
2019-03-22 13:01:38 +0000

    don't construct appinsights with TelemetryClient instance

--- src/main/java/uk/gov/hmcts/reform/em/annotation/config/logging/AppInsights.java
+++ src/main/java/uk/gov/hmcts/reform/em/annotation/config/logging/AppInsights.java
@@ -6,18 +6,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
-import uk.gov.hmcts.reform.logging.appinsights.AbstractAppInsights;
 
 @Component
 @ConditionalOnProperty("azure.app_insights_key")
 public class AppInsights {
 
     private static final Logger LOG = LoggerFactory.getLogger(AppInsights.class);
-    private TelemetryClient client;
 
     @Autowired
     public AppInsights(TelemetryClient client) {
-        this.client = client;
         LOG.info("Building AppInsights");
     }
 }
