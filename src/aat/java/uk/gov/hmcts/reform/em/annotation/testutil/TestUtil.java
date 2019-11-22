ff529dadabc768bad10f431385490008fe53e7fb
Pawel Kaczmarek <pawel.kacmarek@solirius.com>
2019-11-22 10:43:32 +0100

    removed output

--- src/aat/java/uk/gov/hmcts/reform/em/annotation/testutil/TestUtil.java
+++ src/aat/java/uk/gov/hmcts/reform/em/annotation/testutil/TestUtil.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.em.annotation.testutil;
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.em.test.idam.IdamHelper;
 import uk.gov.hmcts.reform.em.test.s2s.S2sHelper;
@@ -21,20 +20,8 @@ public class TestUtil {
     @Autowired
     private S2sHelper s2sHelper;
 
-    @Value("${test.url}")
-    private String testUrl;
-    @Value("${idam.api.url}")
-    private String idamAPIUrl;
-    @Value("${idam.client.id}")
-    private String idamClientId;
-    @Value("${idam.client.secret}")
-    private String idamClientSecret;
-    @Value("${idam.client.redirect_uri}")
-    private String idamClientRedirectUrl;
-
     @PostConstruct
     void postConstruct() {
-        printAllEnv();
         RestAssured.useRelaxedHTTPSValidation();
         idamHelper.createUser("a@b.com", Stream.of("caseworker").collect(Collectors.toList()));
     }
@@ -46,12 +33,4 @@ public class TestUtil {
             .header("ServiceAuthorization", s2sHelper.getS2sToken());
     }
 
-    private void printAllEnv() {
-        System.out.println(String.format("SYS_VARS"));
-        System.out.println(String.format("testUrl=%s", testUrl));
-        System.out.println(String.format("idamAPIUrl=%s", idamAPIUrl));
-        System.out.println(String.format("idamClientId=%s", idamClientId));
-        System.out.println(String.format("idamClientSecret=%s", idamClientSecret));
-        System.out.println(String.format("idamClientRedirectUrl=%s", idamClientRedirectUrl));
-    }
 }
