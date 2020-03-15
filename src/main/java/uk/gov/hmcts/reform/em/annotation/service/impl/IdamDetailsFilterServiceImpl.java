c41b6cfa9c761ecf6a26b94b003ea47702750a0a
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-03-15 21:56:42 +0000

    Resolved sonar issues

--- src/main/java/uk/gov/hmcts/reform/em/annotation/service/impl/IdamDetailsFilterServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/em/annotation/service/impl/IdamDetailsFilterServiceImpl.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.em.annotation.service.impl;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.reform.em.annotation.domain.IdamDetails;
@@ -20,7 +21,8 @@ public class IdamDetailsFilterServiceImpl implements IdamDetailsFilterService {
     @Override
     public void saveIdamDetails(UserDetails userDetails) {
 
-        if (!idamDetailsRepository.existsById(userDetails.getId())) {
+        if (StringUtils.isNotBlank(userDetails.getId())
+                && !idamDetailsRepository.existsById(userDetails.getId())) {
             IdamDetails idamDetails = new IdamDetails();
             idamDetails.setId(userDetails.getId());
             idamDetails.setForename(userDetails.getForename());
