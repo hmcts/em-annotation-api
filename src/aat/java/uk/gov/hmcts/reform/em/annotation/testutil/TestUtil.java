f8785602620d0576982f42207f36d69f190c09cb
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-03-16 20:57:02 +0000

    Added in Test cases on behalf of Kasi

--- src/aat/java/uk/gov/hmcts/reform/em/annotation/testutil/TestUtil.java
+++ src/aat/java/uk/gov/hmcts/reform/em/annotation/testutil/TestUtil.java
@@ -1,15 +1,17 @@
 package uk.gov.hmcts.reform.em.annotation.testutil;
 
+import uk.gov.hmcts.reform.em.test.idam.IdamHelper;
+import uk.gov.hmcts.reform.em.test.s2s.S2sHelper;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.annotation.PostConstruct;
+
 import io.restassured.RestAssured;
 import io.restassured.specification.RequestSpecification;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import uk.gov.hmcts.reform.em.test.idam.IdamHelper;
-import uk.gov.hmcts.reform.em.test.s2s.S2sHelper;
-
-import javax.annotation.PostConstruct;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class TestUtil {
@@ -20,17 +22,70 @@ public class TestUtil {
     @Autowired
     private S2sHelper s2sHelper;
 
+    private String idamAuth;
+    private String s2sAuth;
+
     @PostConstruct
     void postConstruct() {
         RestAssured.useRelaxedHTTPSValidation();
         idamHelper.createUser("a@b.com", Stream.of("caseworker").collect(Collectors.toList()));
+        idamAuth = idamHelper.authenticateUser("a@b.com");
+        s2sAuth = s2sHelper.getS2sToken();
+
     }
 
     public RequestSpecification authRequest() {
         return RestAssured
-            .given()
-            .header("Authorization", idamHelper.authenticateUser("a@b.com"))
-            .header("ServiceAuthorization", s2sHelper.getS2sToken());
+                .given()
+                .header("Authorization", idamHelper.authenticateUser("a@b.com"))
+                .header("ServiceAuthorization", s2sHelper.getS2sToken());
+    }
+
+    public RequestSpecification emptyIdamAuthRequest() {
+        return s2sAuthRequest()
+                .header("Authorization", null);
+    }
+
+    public RequestSpecification emptyIdamAuthAndEmptyS2SAuth() {
+        return RestAssured
+                .given()
+                .header("ServiceAuthorization", null)
+                .header("Authorization", null);
+    }
+
+    public RequestSpecification validAuthRequestWithEmptyS2SAuth() {
+        return emptyS2sAuthRequest().header("Authorization", idamAuth);
+    }
+
+    public RequestSpecification validS2SAuthWithEmptyIdamAuth() {
+
+        return s2sAuthRequest().header("Authorization", null);
+    }
+
+    private RequestSpecification emptyS2sAuthRequest() {
+
+        return RestAssured.given().header("ServiceAuthorization", null);
+    }
+
+    public RequestSpecification invalidIdamAuthrequest() {
+
+        return s2sAuthRequest().header("Authorization", "invalidIDAMAuthRequest");
+    }
+
+    public RequestSpecification invalidS2SAuth() {
+
+        return invalidS2sAuthRequest().header("Authorization", idamAuth);
+    }
+
+    private RequestSpecification invalidS2sAuthRequest() {
+
+        return RestAssured.given().header("ServiceAuthorization", "invalidS2SAuthorization");
+    }
+
+    private RequestSpecification s2sAuthRequest() {
+        return RestAssured
+                .given()
+                .header("ServiceAuthorization", s2sAuth);
     }
 
 }
