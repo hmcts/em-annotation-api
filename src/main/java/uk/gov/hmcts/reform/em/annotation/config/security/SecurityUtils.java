1d18bc684d6481be6549fe86a81c1f49d37eaaa1
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-03-17 09:07:47 +0000

    Added in logging

--- src/main/java/uk/gov/hmcts/reform/em/annotation/config/security/SecurityUtils.java
+++ src/main/java/uk/gov/hmcts/reform/em/annotation/config/security/SecurityUtils.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.em.annotation.config.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -21,6 +23,8 @@ import static org.springframework.security.oauth2.core.endpoint.OAuth2ParameterN
 @Service
 public class SecurityUtils {
 
+    private final Logger log = LoggerFactory.getLogger(SecurityUtils.class);
+
     public static final String TOKEN_NAME = "tokenName";
 
     private final IdamRepository idamRepository;
@@ -39,18 +43,24 @@ public class SecurityUtils {
         SecurityContext securityContext = SecurityContextHolder.getContext();
         return Optional.ofNullable(securityContext.getAuthentication())
                 .map(authentication -> {
+                    log.info(" ================== Principal is : "+ authentication.getPrincipal());
                     if (authentication.getPrincipal() instanceof UserDetails) {
                         UserDetails springSecurityUser = (UserDetails) authentication.getPrincipal();
+                        log.info(" ============= UserDetails Principal is : "+ springSecurityUser.getUsername());
                         return springSecurityUser.getUsername();
                     } else if (authentication.getPrincipal() instanceof String) {
+                        log.info(" ================== String Principal is : "+ authentication.getPrincipal());
                         return (String) authentication.getPrincipal();
                     } else if (authentication instanceof JwtAuthenticationToken) {
+
                         Jwt jwt = ((JwtAuthenticationToken) authentication).getToken();
+                        log.info(" ================== JwtAuthenticationToken is : "+ jwt);
                         if (jwt.containsClaim(TOKEN_NAME) && jwt.getClaim(TOKEN_NAME).equals(ACCESS_TOKEN)) {
                             uk.gov.hmcts.reform.idam.client.models.UserDetails userDetails = idamRepository.getUserDetails(jwt.getTokenValue());
                             return userDetails.getId();
                         }
                     } else if (authentication.getPrincipal() instanceof DefaultOidcUser) {
+                        log.info(" ================== DefaultOidcUser is : "+ authentication.getPrincipal());
                         Map<String, Object> attributes = ((DefaultOidcUser) authentication.getPrincipal()).getAttributes();
                         if (attributes.containsKey("preferred_username")) {
                             return (String) attributes.get("preferred_username");
