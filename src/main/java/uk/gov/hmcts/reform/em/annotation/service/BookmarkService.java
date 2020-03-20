08adf679e59959ed606f47549757a79e03a6f34e
tomxelliott <tomelliott@Toms-MBP.home>
2020-03-20 12:43:07 +0000

    EM-2687: Add entity table for Bookmarks and start writing integration tests

--- src/main/java/uk/gov/hmcts/reform/em/annotation/service/BookmarkService.java
+++ src/main/java/uk/gov/hmcts/reform/em/annotation/service/BookmarkService.java
@@ -1,4 +1,34 @@
 package uk.gov.hmcts.reform.em.annotation.service;
 
-public class BookmarkService {
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import uk.gov.hmcts.reform.em.annotation.service.dto.BookmarkDTO;
+
+import java.util.UUID;
+
+public interface BookmarkService {
+
+    /**
+     * Save a bookmark.
+     *
+     * @param bookmarkDTO the entity to save
+     * @return the persisted entity
+     */
+    BookmarkDTO save(BookmarkDTO bookmarkDTO);
+
+    /**
+     *
+     *
+     * @param pageable
+     * @param documentId
+     * @return
+     */
+    Page<BookmarkDTO> findAllByDocumentId(String documentId, Pageable pageable);
+
+    /**
+     * Delete the "id" bookmark.
+     *
+     * @param id the id of the entity
+     */
+    void delete(UUID id);
 }
