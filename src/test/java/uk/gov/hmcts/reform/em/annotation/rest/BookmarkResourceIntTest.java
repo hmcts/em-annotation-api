fb506b26fcee4f2039593f59aad40791303b1d00
tomxelliott <tomelliott@Toms-MBP.home>
2020-03-20 17:08:01 +0000

    EM-2687: Add test cases

--- src/test/java/uk/gov/hmcts/reform/em/annotation/rest/BookmarkResourceIntTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/annotation/rest/BookmarkResourceIntTest.java
@@ -25,8 +25,8 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * Test class for the CommentResource REST controller.
@@ -101,5 +101,79 @@ public class BookmarkResourceIntTest extends BaseTest {
         // Validate the Comment in the database
         List<Bookmark> bookmarkList = bookmarkRepository.findAll();
         assertThat(bookmarkList).hasSize(databaseSizeBeforeCreate);
+    }
+
+    @Test
+    @Transactional
+    public void createBookmarkWithExistingId() throws Exception {
+        int databaseSizeBeforeCreate = bookmarkRepository.findAll().size();
+
+        bookmark.setId(UUID.randomUUID());
+        BookmarkDTO bookmarkDTO = bookmarkMapper.toDto(bookmark);
+
+        restLogoutMockMvc.perform(post("/api/bookmarks")
+                .contentType(TestUtil.APPLICATION_JSON_UTF8)
+                .content(TestUtil.convertObjectToJsonBytes(bookmarkDTO)))
+                .andExpect(status().isCreated());
+
+        List<Bookmark> bookmarkList = bookmarkRepository.findAll();
+        assertThat(bookmarkList).hasSize(databaseSizeBeforeCreate + 1);
+    }
+
+    @Test
+    @Transactional
+    public void updateBookmark() throws Exception {
+        bookmarkRepository.saveAndFlush(bookmark);
+
+        int databaseSizeBeforeUpdate = bookmarkRepository.findAll().size();
+
+        Bookmark updatedBookmark = bookmarkRepository.findById(bookmark.getId()).get();
+
+        em.detach(updatedBookmark);
+        updatedBookmark
+                .setName("Updated Bookmark");
+        BookmarkDTO bookmarkDTO = bookmarkMapper.toDto(updatedBookmark);
+
+        restLogoutMockMvc.perform(put("/api/bookmarks")
+                .contentType(TestUtil.APPLICATION_JSON_UTF8)
+                .content(TestUtil.convertObjectToJsonBytes(bookmarkDTO)))
+                .andExpect(status().isOk());
+
+        // Validate the Comment in the database
+        List<Bookmark> bookmarkList = bookmarkRepository.findAll();
+        assertThat(bookmarkList).hasSize(databaseSizeBeforeUpdate);
+        Bookmark testBookmark = bookmarkList.get(bookmarkList.size() - 1);
+        assertThat(testBookmark.getName()).isEqualTo("Updated Bookmark");
+    }
+
+    @Test
+    @Transactional
+    public void updateNonExistentBookmark() throws Exception {
+        int databaseSizeBeforeUpdate = bookmarkRepository.findAll().size();
+        bookmark.setId(null);
+
+        BookmarkDTO bookmarkDTO = bookmarkMapper.toDto(bookmark);
+
+        restLogoutMockMvc.perform(put("/api/bookmarks")
+                .contentType(TestUtil.APPLICATION_JSON_UTF8)
+                .content(TestUtil.convertObjectToJsonBytes(bookmarkDTO)))
+                .andExpect(status().isBadRequest());
+
+        List<Bookmark> bookmarkList = bookmarkRepository.findAll();
+        assertThat(bookmarkList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    @Transactional
+    public void deleteBookmark() throws Exception {
+        bookmarkRepository.saveAndFlush(bookmark);
+
+        int databaseSizeBeforeDelete = bookmarkRepository.findAll().size();
+        restLogoutMockMvc.perform(delete("/api/bookmarks/{id}", bookmark.getId())
+                .accept(TestUtil.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk());
+
+        List<Bookmark> bookmarkList = bookmarkRepository.findAll();
+        assertThat(bookmarkList).hasSize(databaseSizeBeforeDelete - 1);
     }
 }
