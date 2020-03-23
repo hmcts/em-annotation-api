0034e7030681604180f9f6f22ec3888a500d10b2
tomxelliott <tomelliott@Toms-MBP.home>
2020-03-23 14:20:59 +0000

    EM-2649: Return 204 empty response if there are no annotation sets for a document

--- src/main/java/uk/gov/hmcts/reform/em/annotation/rest/errors/ResourceNotFoundException.java
+++ src/main/java/uk/gov/hmcts/reform/em/annotation/rest/errors/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package uk.gov.hmcts.reform.em.annotation.rest.errors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@ResponseStatus(value = HttpStatus.NO_CONTENT)
 public class ResourceNotFoundException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
