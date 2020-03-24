83ef7d9e559cd1cf360b766c45d96af207bbc514
tomxelliott <tomelliott@Toms-MBP.home>
2020-03-24 16:56:03 +0000

    Revert back the exception http code

--- src/main/java/uk/gov/hmcts/reform/em/annotation/rest/errors/ResourceNotFoundException.java
+++ src/main/java/uk/gov/hmcts/reform/em/annotation/rest/errors/ResourceNotFoundException.java
@@ -3,7 +3,7 @@ package uk.gov.hmcts.reform.em.annotation.rest.errors;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NO_CONTENT)
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
     private static final long serialVersionUID = 1L;
