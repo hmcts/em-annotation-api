ad63c6474a57be5233ac03af9e071082d109c4e5
tomxelliott <tomelliott@Toms-MBP.home>
2020-03-26 16:42:28 +0000

    EM-2718: Changes for sonar

--- src/main/java/uk/gov/hmcts/reform/em/annotation/service/dto/AnnotationDTO.java
+++ src/main/java/uk/gov/hmcts/reform/em/annotation/service/dto/AnnotationDTO.java
@@ -1,9 +1,7 @@
 package uk.gov.hmcts.reform.em.annotation.service.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import uk.gov.hmcts.reform.em.annotation.domain.enumeration.AnnotationType;
 
 import java.io.Serializable;
 import java.util.HashSet;
@@ -27,14 +25,22 @@ public class AnnotationDTO extends AbstractAuditingDTO implements Serializable {
 
     private UUID annotationSetId;
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    private String documentId;
-
     private Set<CommentDTO> comments = new HashSet<>();
 
     private Set<TagDTO> tags = new HashSet<>();
 
     private Set<RectangleDTO> rectangles = new HashSet<>();
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String documentId;
+
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
 
     public UUID getId() {
         return id;
@@ -100,16 +106,7 @@ public class AnnotationDTO extends AbstractAuditingDTO implements Serializable {
         this.color = color;
     }
 
-    public String getDocumentId() {
-        return documentId;
-    }
-
-    public void setDocumentId(String documentId) {
-        this.documentId = documentId;
-    }
-
     @Override
-
     public boolean equals(Object o) {
         if (this == o) {
             return true;
