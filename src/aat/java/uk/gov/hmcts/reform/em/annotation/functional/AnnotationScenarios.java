d26beaf00d65c97f51b417b51a357183426a3474
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2020-05-03 10:08:46 +0100

    Added SerenityIntegrationSpringRunner in Functional Tests.

--- src/aat/java/uk/gov/hmcts/reform/em/annotation/functional/AnnotationScenarios.java
+++ src/aat/java/uk/gov/hmcts/reform/em/annotation/functional/AnnotationScenarios.java
@@ -10,9 +10,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.em.EmTestConfig;
 import uk.gov.hmcts.reform.em.annotation.testutil.TestUtil;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+
 
 import java.util.UUID;
 
@@ -21,7 +22,7 @@ import static org.hamcrest.CoreMatchers.is;
 
 @SpringBootTest(classes = {TestUtil.class, EmTestConfig.class})
 @PropertySource(value = "classpath:application.yml")
-@RunWith(SpringRunner.class)
+@RunWith(SpringIntegrationSerenityRunner.class)
 public class AnnotationScenarios {
 
     @Autowired
