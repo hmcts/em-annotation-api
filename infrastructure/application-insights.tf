14850dd593bde647e3926a3baa858c3a44fe63b3
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-09-30 10:44:35 +0100

    Resolved build issue

--- infrastructure/application-insights.tf
+++ infrastructure/application-insights.tf
@@ -1,8 +1,8 @@
 resource "azurerm_application_insights" "appinsights" {
   name                = "${var.product}-${var.env}"
-  location            = "${var.appinsights_location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  application_type    = "${var.application_type}"
+  location            = var.appinsights_location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = var.application_type
 }
 
 output "appInsightsInstrumentationKey" {
