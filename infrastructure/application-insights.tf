b9305eba465f436ce4030132966b83a6dd27f64f
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-10-01 09:05:00 +0100

    updated appinsights details

--- infrastructure/application-insights.tf
+++ infrastructure/application-insights.tf
@@ -3,6 +3,16 @@ resource "azurerm_application_insights" "appinsights" {
   location            = var.appinsights_location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = var.application_type
+
+  tags = var.common_tags
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
+      # destroys and re-creates this appinsights instance
+      application_type,
+    ]
+  }
 }
 
 output "appInsightsInstrumentationKey" {
