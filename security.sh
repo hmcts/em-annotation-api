361500af79550a75e5e56ef2f9eca80f42bf6fbd
PhaniP <phani.perla@hmcts.net>
2021-08-31 14:31:06 +0100

    updated security scan script

--- security.sh
+++ security.sh
@@ -4,6 +4,7 @@ zap-api-scan.py -t ${TEST_URL}/v2/api-docs -f openapi -s -S -d -u ${SecurityRule
 curl --fail http://0.0.0.0:1001/OTHER/core/other/jsonreport/?formMethod=GET --output report.json
 export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
+
 cat zap.out
 echo "ZAP has successfully started"
 zap-cli --zap-url http://0.0.0.0 -p 1001 report -o /zap/api-report.html -f html
@@ -11,4 +12,6 @@ zap-cli --zap-url http://0.0.0.0 -p 1001 alerts -l High --exit-code False
 cp /zap/api-report.html functional-output/
 mkdir -p functional-output
 chmod a+wx functional-output
+cp /zap/api-report.html functional-output/
+cp *.* functional-output/
 
