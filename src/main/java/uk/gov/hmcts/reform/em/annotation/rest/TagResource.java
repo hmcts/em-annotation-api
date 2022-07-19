43f18473f2cb32c328106c7aa3813bdb652a6138
matt-k1998 <matt.kolakowski@hotmail.com>
2022-07-19 17:06:07 +0100

    remove unused import

--- src/main/java/uk/gov/hmcts/reform/em/annotation/rest/TagResource.java
+++ src/main/java/uk/gov/hmcts/reform/em/annotation/rest/TagResource.java
@@ -9,7 +9,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
