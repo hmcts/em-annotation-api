feee78c109ab211024e00fecab27e84c66e1d312
yogesh-hullatti <53007108+yogesh-hullatti@users.noreply.github.com>
2022-10-12 09:51:15 +0100

    Updated log for ExceptionTranslator to info

--- src/main/java/uk/gov/hmcts/reform/em/annotation/rest/errors/ExceptionTranslator.java
+++ src/main/java/uk/gov/hmcts/reform/em/annotation/rest/errors/ExceptionTranslator.java
@@ -169,7 +169,7 @@ public class ExceptionTranslator implements ProblemHandling {
 
     @ExceptionHandler
     public ResponseEntity<Problem> handlePSQLException(PSQLException ex, NativeWebRequest request) {
-        log.error("Em-Annotation sql exception : {} ",ex.getMessage());
+        log.info("Em-Annotation sql exception : {} ",ex.getMessage());
         if (ex.getMessage().contains("duplicate key value violates unique constraint")) {
             Problem problem = Problem.builder()
                     .withStatus(Status.CONFLICT)
