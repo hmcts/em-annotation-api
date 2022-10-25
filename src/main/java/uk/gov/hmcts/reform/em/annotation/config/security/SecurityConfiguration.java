c35e7da44477704151fb5c9612a9c56e7197eb3c
michaeljacob <Michael.Jacob@HMCTS.NET>
2022-10-25 10:39:17 +0100

    Remove comments

--- src/main/java/uk/gov/hmcts/reform/em/annotation/config/security/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/annotation/config/security/SecurityConfiguration.java
@@ -78,10 +78,7 @@ public class SecurityConfiguration {
     JwtDecoder jwtDecoder() {
         NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder)
                 JwtDecoders.fromOidcIssuerLocation(issuerUri);
-        // We are using issuerOverride instead of issuerUri as SIDAM has the wrong issuer at the moment
         OAuth2TokenValidator<Jwt> withTimestamp = new JwtTimestampValidator();
-        //Add the JwtIssuerValidator back in after Idam issue is resolved
-        //OAuth2TokenValidator<Jwt> withIssuer = new JwtIssuerValidator(issuerOverride);
         OAuth2TokenValidator<Jwt> validator = new DelegatingOAuth2TokenValidator<>(withTimestamp);
 
         jwtDecoder.setJwtValidator(validator);
