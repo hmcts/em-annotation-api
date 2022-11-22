2a0deb591857e7e50fcb0aedf0286cf79dd253f4
SomeshRA <somesh.acharya1@hmcts.net>
2022-11-22 16:35:49 +0000

    EM-4813: Enforced baseline migration

--- src/main/java/uk/gov/hmcts/reform/em/annotation/data/migration/FlywayNoOpStrategy.java
+++ src/main/java/uk/gov/hmcts/reform/em/annotation/data/migration/FlywayNoOpStrategy.java
@@ -1,14 +1,20 @@
 package uk.gov.hmcts.reform.em.annotation.data.migration;
 
+import com.sun.istack.logging.Logger;
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 
 import java.util.stream.Stream;
 
 public class FlywayNoOpStrategy implements FlywayMigrationStrategy {
-
+    Logger log = Logger.getLogger(FlywayNoOpStrategy.class);
     @Override
     public void migrate(Flyway flyway) {
+        log.info("Database before migration done {}", flyway.info().all());
+        Flyway.configure()
+                .baselineOnMigrate(true)
+                .load();
+        log.info("Database migration done {}", flyway.info().all());
         Stream.of(flyway.info().all())
                 .filter(info -> !info.getState().isApplied())
                 .findFirst()
