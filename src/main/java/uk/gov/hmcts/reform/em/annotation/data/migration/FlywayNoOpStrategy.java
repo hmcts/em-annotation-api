6b8536ee6eb32cf4c2f24c6c32f369d1a9960a76
SomeshRA <somesh.acharya1@hmcts.net>
2022-11-23 11:53:58 +0000

    EM-4813: Added checks to ignore BASELINE and BASELINE_IGNORED state

--- src/main/java/uk/gov/hmcts/reform/em/annotation/data/migration/FlywayNoOpStrategy.java
+++ src/main/java/uk/gov/hmcts/reform/em/annotation/data/migration/FlywayNoOpStrategy.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 
 import java.util.stream.Stream;
 
+import static org.flywaydb.core.api.MigrationState.BASELINE;
 import static org.flywaydb.core.api.MigrationState.BASELINE_IGNORED;
 
 public class FlywayNoOpStrategy implements FlywayMigrationStrategy {
@@ -17,11 +18,13 @@ public class FlywayNoOpStrategy implements FlywayMigrationStrategy {
         log.info("FlywayNoOpStrategy invoked to migrate");
 
         Stream.of(flyway.info().all())
-                .peek(migrationInfo -> log.info("Info of script {} : state {}", migrationInfo.getScript(), migrationInfo.getState()))
-                .filter(info -> !BASELINE_IGNORED.equals(info.getState()) && !info.getState().isApplied())
-                .findFirst()
-                .ifPresent(info -> {
-                    throw new PendingMigrationScriptException(info.getScript());
-                });
+            .peek(migrationInfo -> log.info("Info of script {} : state {}", migrationInfo.getScript(), migrationInfo.getState()))
+            .filter(info -> !BASELINE.equals(info.getState())
+                && !BASELINE_IGNORED.equals(info.getState())
+                && !info.getState().isApplied())
+            .findFirst()
+            .ifPresent(info -> {
+                throw new PendingMigrationScriptException(info.getScript());
+            });
     }
 }
