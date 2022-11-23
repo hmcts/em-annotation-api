f1fa19b23ce2618d7a62f4e23ac5780d935770cd
SomeshRA <somesh.acharya1@hmcts.net>
2022-11-23 10:08:04 +0000

    EM-4813: Ignoring baseline script

--- src/main/java/uk/gov/hmcts/reform/em/annotation/data/migration/FlywayNoOpStrategy.java
+++ src/main/java/uk/gov/hmcts/reform/em/annotation/data/migration/FlywayNoOpStrategy.java
@@ -1,19 +1,22 @@
 package uk.gov.hmcts.reform.em.annotation.data.migration;
 
-import com.sun.istack.logging.Logger;
 import org.flywaydb.core.Flyway;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 
 import java.util.stream.Stream;
 
 public class FlywayNoOpStrategy implements FlywayMigrationStrategy {
-    Logger log = Logger.getLogger(FlywayNoOpStrategy.class);
+    Logger log = LoggerFactory.getLogger(FlywayNoOpStrategy.class);
 
     @Override
     public void migrate(Flyway flyway) {
         log.info("FlywayNoOpStrategy invoked");
 
         Stream.of(flyway.info().all())
+                .peek(migrationInfo -> log.info("detailed info {}", migrationInfo))
+                .filter(info -> !"V1__baseline_migration.sql".equals(info.getScript()))
                 .filter(info -> !info.getState().isApplied())
                 .findFirst()
                 .ifPresent(info -> {
