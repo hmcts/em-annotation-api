9c4574a463e90e38270b8adfeb8b86c7d4c3da52
SomeshRA <somesh.acharya1@hmcts.net>
2022-11-23 12:00:57 +0000

    EM-4813: Removed logging

--- src/main/java/uk/gov/hmcts/reform/em/annotation/data/migration/FlywayNoOpStrategy.java
+++ src/main/java/uk/gov/hmcts/reform/em/annotation/data/migration/FlywayNoOpStrategy.java
@@ -1,8 +1,6 @@
 package uk.gov.hmcts.reform.em.annotation.data.migration;
 
 import org.flywaydb.core.Flyway;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 
 import java.util.stream.Stream;
@@ -11,14 +9,10 @@ import static org.flywaydb.core.api.MigrationState.BASELINE;
 import static org.flywaydb.core.api.MigrationState.BASELINE_IGNORED;
 
 public class FlywayNoOpStrategy implements FlywayMigrationStrategy {
-    Logger log = LoggerFactory.getLogger(FlywayNoOpStrategy.class);
 
     @Override
     public void migrate(Flyway flyway) {
-        log.info("FlywayNoOpStrategy invoked to migrate");
-
         Stream.of(flyway.info().all())
-            .peek(migrationInfo -> log.info("Info of script {} : state {}", migrationInfo.getScript(), migrationInfo.getState()))
             .filter(info -> !BASELINE.equals(info.getState())
                 && !BASELINE_IGNORED.equals(info.getState())
                 && !info.getState().isApplied())
