05548f2511bf6f499262b4cb5d5865c6874258f6
Duarte Pires Lopes <49475154+Jefex7@users.noreply.github.com>
2023-04-18 14:37:28 +0100

    Welcome Endpoint to return Json format (#1292)

--- src/test/java/uk/gov/hmcts/reform/em/annotation/rest/WelcomeResourceTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/annotation/rest/WelcomeResourceTest.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
@@ -13,13 +15,24 @@ public class WelcomeResourceTest {
     private final WelcomeResource welcomeResource = new WelcomeResource();
 
     @Test
-    public void test_should_return_welcome_response() {
-
-        ResponseEntity<String> responseEntity = welcomeResource.welcome();
-        String expectedMessage = "Welcome to EM Annotation API!";
+    public void testEndPointResponseCode() {
+        ResponseEntity<Map<String,String>> responseEntity = welcomeResource.welcome();
 
         assertNotNull(responseEntity);
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        assertThat(responseEntity.getBody()).contains(expectedMessage);
+    }
+
+    @Test
+    public void testEndpointResponseMessage() {
+        ResponseEntity<Map<String,String>> responseEntity = welcomeResource.welcome();
+
+        Map<String,String> expectedResponse = new HashMap<>();
+        expectedResponse.put("message","Welcome to EM Annotation API!");
+
+        String cacheHeader = responseEntity.getHeaders().getCacheControl();
+
+        assertNotNull(responseEntity);
+        assertEquals("no-cache",cacheHeader);
+        assertEquals(expectedResponse, responseEntity.getBody());
     }
 }
