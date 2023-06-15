017dc90903a79db91db630616af55e21575a1fb0
mdayican <mdayican@gmail.com>
2023-06-15 17:35:07 +0100

    test-2

--- src/aat/java/uk/gov/hmcts/reform/em/annotation/testutil/TestUtil.java
+++ src/aat/java/uk/gov/hmcts/reform/em/annotation/testutil/TestUtil.java
@@ -4,7 +4,8 @@ import io.restassured.specification.RequestSpecification;
 import jakarta.annotation.PostConstruct;
 import net.serenitybdd.rest.SerenityRest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
 import uk.gov.hmcts.reform.em.test.idam.IdamHelper;
 import uk.gov.hmcts.reform.em.test.s2s.S2sHelper;
@@ -14,7 +15,8 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 @Service
-@Configuration
+@ComponentScan({"uk.gov.hmcts.reform.em.test.idam", "uk.gov.hmcts.reform.em.test.s2s"})
+@EnableAutoConfiguration
 public class TestUtil {
 
     @Autowired
