5d717ddad8cc1253761be926ba6918d044d64b2a
michaeljacob <Michael.Jacob@HMCTS.NET>
2023-06-22 15:55:48 +0100

    add user role

--- src/aat/java/uk/gov/hmcts/reform/em/annotation/testutil/TestUtil.java
+++ src/aat/java/uk/gov/hmcts/reform/em/annotation/testutil/TestUtil.java
@@ -29,7 +29,7 @@ public class TestUtil {
     @PostConstruct
     void postConstruct() {
         SerenityRest.useRelaxedHTTPSValidation();
-        idamHelper.createUser("a@b.com", Stream.of("caseworker").collect(Collectors.toList()));
+        idamHelper.createUser("a@b.com", Stream.of("caseworker", "caseworker-sscs").collect(Collectors.toList()));
         idamAuth = idamHelper.authenticateUser("a@b.com");
         s2sAuth = s2sHelper.getS2sToken();
     }
