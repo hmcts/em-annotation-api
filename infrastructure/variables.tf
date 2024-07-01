ea42e7298dbe02c05f0bbbe6eae1314b4b3965ed
58060 <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>
2024-07-01 06:36:10 +0000

    Updating Terraform Formatting

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,93 +1,93 @@
-variable product {
+variable "product" {
   default = "em"
 }
 
-variable shared_product_name {
+variable "shared_product_name" {
   default = "rpa"
 }
 
-variable component {
+variable "component" {
   default = "anno"
 }
 
-variable location {
+variable "location" {
   default = "UK South"
 }
 
-variable env {
+variable "env" {
 }
 
-variable subscription {
+variable "subscription" {
 }
 
-variable tenant_id {}
+variable "tenant_id" {}
 
-variable jenkins_AAD_objectId {
+variable "jenkins_AAD_objectId" {
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
-variable common_tags {
+variable "common_tags" {
   type = map(string)
 }
 ////////////////////////////////////////////////
 //Addtional Vars ///////////////////////////////
 ////////////////////////////////////////////////
-variable capacity {
+variable "capacity" {
   default = "1"
 }
 
-variable java_opts {
+variable "java_opts" {
   default = ""
 }
 ////////////////////////////////////////////////
 // Endpoints
 ////////////////////////////////////////////////
-variable idam_api_base_uri {
+variable "idam_api_base_uri" {
   default = "http://betaDevBccidamAppLB.reform.hmcts.net:80"
 }
 
-variable open_id_api_base_uri {
+variable "open_id_api_base_uri" {
   default = "idam-api"
 }
 
-variable oidc_issuer_base_uri {
+variable "oidc_issuer_base_uri" {
   default = "idam-api"
 }
 
-variable s2s_url {
+variable "s2s_url" {
   default = "rpe-service-auth-provider"
 }
 
-variable dm_store_app_url {
+variable "dm_store_app_url" {
   default = "dm-store"
 }
 
-variable em_anno_app_url {
+variable "em_anno_app_url" {
   default = "em-anno"
 }
 
-variable managed_identity_object_id {
+variable "managed_identity_object_id" {
   default = ""
 }
 
-variable appinsights_location {
+variable "appinsights_location" {
   default     = "West Europe"
   description = "Location for Application Insights"
 }
 
-variable application_type {
+variable "application_type" {
   default     = "web"
   description = "Type of Application Insights (Web/Other)"
 }
 ////////////////////////////////////////////////
 // Toggle Features
 ////////////////////////////////////////////////
-variable enable_idam_healthcheck {
-    default = "false"
+variable "enable_idam_healthcheck" {
+  default = "false"
 }
 
-variable enable_s2s_healthcheck {
-    default = "false"
+variable "enable_s2s_healthcheck" {
+  default = "false"
 }
 
 /// v15 DB Details
