f83a94b983e9057ec0b54de99cdb3c2b6085e70a
MichaelJacob21012 <88201349+MichaelJacob21012@users.noreply.github.com>
2024-11-26 11:22:03 +0100

    update serenity to 4.2.9 (#2157)

--- src/aat/java/uk/gov/hmcts/reform/em/annotation/functional/OpenIdConnectScenarios.java
+++ src/aat/java/uk/gov/hmcts/reform/em/annotation/functional/OpenIdConnectScenarios.java
@@ -1,8 +1,8 @@
 package uk.gov.hmcts.reform.em.annotation.functional;
 
+import net.serenitybdd.annotations.WithTag;
+import net.serenitybdd.annotations.WithTags;
 import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
-import net.thucydides.core.annotations.WithTag;
-import net.thucydides.core.annotations.WithTags;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
