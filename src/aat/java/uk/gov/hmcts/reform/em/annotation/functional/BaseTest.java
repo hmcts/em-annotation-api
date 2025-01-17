806469d922c217e7375f75cee8a542604caa4af1
MichaelJacob21012 <88201349+MichaelJacob21012@users.noreply.github.com>
2025-01-17 12:31:42 +0100

    upgrade em-test-helper to 2.1.1 (#2238)
    * upgrade em-test-helper to 2.1.1
    
    * add BaseTest

---
+++ src/aat/java/uk/gov/hmcts/reform/em/annotation/functional/BaseTest.java
@@ -0,0 +1,50 @@
+package uk.gov.hmcts.reform.em.annotation.functional;
+
+import io.restassured.specification.RequestSpecification;
+import net.serenitybdd.annotations.WithTag;
+import net.serenitybdd.annotations.WithTags;
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.em.annotation.testutil.TestUtil;
+import uk.gov.hmcts.reform.em.test.retry.RetryExtension;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@SpringBootTest(classes = {TestUtil.class})
+@TestPropertySource(value = "classpath:application.yml")
+@ExtendWith({SerenityJUnit5Extension.class, SpringExtension.class})
+@WithTags({@WithTag("testType:Functional")})
+public abstract class BaseTest {
+
+    @Autowired
+    protected TestUtil testUtil;
+
+    @Value("${test.url}")
+    protected String testUrl;
+
+    @RegisterExtension
+    protected RetryExtension retryExtension = new RetryExtension(3);
+
+    protected RequestSpecification request;
+    protected RequestSpecification unAuthenticatedRequest;
+
+    @BeforeEach
+    public void setupRequestSpecification() {
+        request = testUtil
+            .authRequest()
+            .baseUri(testUrl)
+            .contentType(APPLICATION_JSON_VALUE);
+
+        unAuthenticatedRequest = testUtil
+            .unauthenticatedRequest()
+            .baseUri(testUrl)
+            .contentType(APPLICATION_JSON_VALUE);
+    }
+}
