88eeff23dc4696e4cfecc9857b05ca4201ead91c
michaeljacob <Michael.Jacob@HMCTS.NET>
2025-05-13 10:56:47 +0100

    clean

--- src/contractTest/java/uk/gov/hmcts/reform/em/annotation/provider/BaseProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/em/annotation/provider/BaseProviderTest.java
@@ -11,8 +11,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.context.annotation.Import;
@@ -43,8 +41,6 @@ public abstract class BaseProviderTest {
 
     protected static final UUID EXAMPLE_USER_ID = UUID.fromString("c38fd29e-fa2e-43d4-a599-2d3f2908565b");
     protected static final Instant EXAMPLE_DATE = Instant.parse("2024-01-15T10:00:00.123Z");
-
-    private static final Logger log = LoggerFactory.getLogger(BaseProviderTest.class);
 
     @BeforeEach
     void setupPactVerification(PactVerificationContext context) {
