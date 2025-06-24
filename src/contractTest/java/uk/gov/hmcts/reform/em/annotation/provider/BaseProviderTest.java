fb929169c942cdf2f252991e34f665102a3d0ecc
michaeljacob <Michael.Jacob@HMCTS.NET>
2025-06-24 17:11:21 +0100

    update selector

--- src/contractTest/java/uk/gov/hmcts/reform/em/annotation/provider/BaseProviderTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/em/annotation/provider/BaseProviderTest.java
@@ -68,7 +68,7 @@ public abstract class BaseProviderTest {
     @PactBrokerConsumerVersionSelectors
     public static SelectorBuilder consumerVersionSelectors() {
         return new SelectorBuilder()
-            .matchingBranch();
+            .mainBranch();
 
     }
 
