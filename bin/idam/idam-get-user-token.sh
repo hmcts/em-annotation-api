7e7fe95291180f26c28164ba145605fab23cd5ca
mdayican <mdayican@gmail.com>
2026-06-08 15:54:28 +0100

    EM-7165 remove deprecated idam calls

--- bin/idam/idam-get-user-token.sh
+++ bin/idam/idam-get-user-token.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-B_T="Authorization: Basic "
-USER_CRED=$(printf "${1}:${2}" | base64)
-AUTH=$B_T$USER_CRED
-echo $(curl -s -X POST -H "${AUTH}" ${3}/oauth2/authorize | jq -r '."access-token"')
+echo $(curl -s -X POST \
+  "${3}/o/token" \
+  -H 'Content-Type: application/x-www-form-urlencoded' \
+  -d "grant_type=password&username=${1}&password=${2}&client_id=${IDAM_CLIENT_ID:-webshow}&client_secret=${IDAM_CLIENT_SECRET:-AAAAAAAAAAAAAAAA}&scope=openid%20roles%20profile&redirect_uri=${IDAM_REDIRECT_URI:-http%3A%2F%2Flocalhost%3A8080%2Foauth2redirect}" \
+  | jq -r '.access_token')
