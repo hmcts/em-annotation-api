2cb806b12d471e9691c34020078e0060fe6146e8
Daniel Lindsay <daniel.lindsay@HMCTS.NET>
2026-09-17 15:56:19 +0100

    CME-1084 Created test-specific security configuration to replace BaseProviderTest's AutoConfigureMockMvc(addFilters = false) annotation which doesn't work with Spring Boot 4

---
+++ src/contractTest/java/uk/gov/hmcts/reform/em/annotation/provider/ContractTestSecurityConfiguration.java
@@ -0,0 +1,22 @@
+package uk.gov.hmcts.reform.em.annotation.provider;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class ContractTestSecurityConfiguration {
+
+    @Bean
+    SecurityFilterChain contractTestSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
