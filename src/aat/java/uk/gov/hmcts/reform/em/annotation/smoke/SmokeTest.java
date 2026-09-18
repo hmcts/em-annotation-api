7ef671bcc502093729c70db5b054a7e39f2909a0
Daniel Lindsay <daniel.lindsay@HMCTS.NET>
2026-09-18 12:36:09 +0100

    CME-1084 Removed smoke test changes

--- src/aat/java/uk/gov/hmcts/reform/em/annotation/smoke/SmokeTest.java
+++ src/aat/java/uk/gov/hmcts/reform/em/annotation/smoke/SmokeTest.java
@@ -7,26 +7,17 @@ import net.serenitybdd.rest.SerenityRest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@ContextConfiguration(
-    classes = SmokeTest.TestConfig.class,
-    initializers = ConfigDataApplicationContextInitializer.class
-)
+@TestPropertySource(value = "classpath:application.yml")
 @ExtendWith({SerenityJUnit5Extension.class, SpringExtension.class})
 @WithTags({@WithTag("testType:Smoke")})
 class SmokeTest {
 
     private static final String MESSAGE = "{\"message\":\"Welcome to EM Annotation API!\"}";
-
-    @TestConfiguration
-    static class TestConfig {
-    }
 
     @Value("${test.url}")
     private String testUrl;
@@ -44,6 +35,7 @@ class SmokeTest {
                         .then()
                         .statusCode(200)
                         .extract()
+                        .body()
                         .asString();
 
         assertEquals(MESSAGE, response);
